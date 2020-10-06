12ad598befccc6cf5b7ef3da445d3fefd96a8238
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2020-10-06 10:04:31 +0200

    no message

--- OENIK_PROG3_2020_2_OXDRAP/Hotel/Program.cs
+++ OENIK_PROG3_2020_2_OXDRAP/Hotel/Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace Hotel
 {
-    class Program
+    /// <summary>
+    /// This is the main program.
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
     }
 }
