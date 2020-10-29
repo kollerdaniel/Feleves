9ce3316064a14f7f896c03abb735177ea0fc2f18
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2020-10-29 11:34:53 +0100

    Recreate project

---
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkiRental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
