11f323da4b16184dabc1726cfb08d15bb786c16b
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2020-11-02 16:06:44 +0100

    Add comments

--- OENIK_PROG3_2020_2_OXDRAP/SkiRental.Repository/SkirentalRepository.cs
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.Repository/SkirentalRepository.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Repository.cs" company="PlaceholderCompany">
+﻿// <copyright file="SkirentalRepository.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
