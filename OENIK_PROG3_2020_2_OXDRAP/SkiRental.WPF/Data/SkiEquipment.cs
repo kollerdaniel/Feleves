bc6bb9e3e6fbd6591578915b12e44bfcb22eec6c
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2021-03-11 23:08:10 +0100

    Added data to WPF

---
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/Data/SkiEquipment.cs
@@ -0,0 +1,78 @@
+﻿// <copyright file="SkiEquipment.cs" company="OXDRAP">
+// Copyright (c) OXDRAP. All rights reserved.
+// </copyright>
+
+namespace SkiRental.WPF.Data
+{
+    using GalaSoft.MvvmLight;
+
+    /// <summary>
+    /// This is the class of the ski equipments.
+    /// </summary>
+    public class SkiEquipment : ObservableObject
+    {
+        private string name;
+
+        private string manufacturer;
+
+        private int price;
+
+        private int size;
+
+        private string difficulty;
+
+        /// <summary>
+        /// Gets or sets the id of the equipments, and this is the primary key.
+        /// </summary>
+        public int SkiEquipmentsId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the basket id of the equipments, and it is a foreign key.
+        /// </summary>
+        public virtual int OrderId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the equipments.
+        /// </summary>
+        public string Name
+        {
+            get { return this.name; } set { this.Set(ref this.name, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the manufacturer of the equipments.
+        /// </summary>
+        public string Manufacturer
+        {
+            get { return this.manufacturer; }
+            set { this.Set(ref this.manufacturer, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the price of the equipments.
+        /// </summary>
+        public int Price
+        {
+            get { return this.price; }
+            set { this.Set(ref this.price, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the equipments.
+        /// </summary>
+        public int Size
+        {
+            get { return this.size; }
+            set { this.Set(ref this.size, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the difficulty of the equipments.
+        /// </summary>
+        public string Difficulty
+        {
+            get { return this.difficulty; }
+            set { this.Set(ref this.difficulty, value); }
+        }
+    }
+}
