9ed75fffdf31aaf947a8fc5e191e4350e7ac687d
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2021-03-11 22:15:47 +0100

    Added file header

--- OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/App.xaml.cs
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/App.xaml.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿// <copyright file="App.xaml.cs" company="OXDRAP">
+// Copyright (c) OXDRAP. All rights reserved.
+// </copyright>
 
 namespace SkiRental.WPF
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
