10c6093810fcaa9d955ea9e8350b84b1434575c7
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2021-03-12 12:49:16 +0100

    Added editor view model

---
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/WM/EditorViewModel.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="EditorViewModel.cs" company="OXDRAP">
+// Copyright (c) OXDRAP. All rights reserved.
+// </copyright>
+
+namespace SkiRental.WPF.WM
+{
+    using GalaSoft.MvvmLight;
+    using SkiRental.WPF.Data;
+
+    /// <summary>
+    /// This is an editer view model.
+    /// </summary>
+    public class EditorViewModel : ViewModelBase
+    {
+        private SkiEquipment skiEquipment;
+
+        /// <summary>
+        /// Gets or sets a ski equipment.
+        /// </summary>
+        public SkiEquipment SkiEquipment
+        {
+            get { return this.skiEquipment; }
+            set { this.Set(ref this.skiEquipment, value); }
+        }
+    }
+}
