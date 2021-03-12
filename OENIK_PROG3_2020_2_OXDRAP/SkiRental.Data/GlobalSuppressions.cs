91dd49c71a549c520c84699bd20dfdab2b105f66
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2021-03-12 19:16:05 +0100

    Added globalsupperessions

---
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.Data/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="OXDRAP">
+// Copyright (c) OXDRAP. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("", "CA1014", Justification = "<NikGitStats>", Scope = "module")]
