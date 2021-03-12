5183bbde84f5529e2d1b48604069dacb5ba82b86
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2021-03-12 17:45:13 +0100

    Edited app.xaml

--- OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/App.xaml.cs
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/App.xaml.cs
@@ -4,18 +4,29 @@
 
 namespace SkiRental.WPF
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Configuration;
-    using System.Data;
-    using System.Linq;
-    using System.Threading.Tasks;
     using System.Windows;
+    using CommonServiceLocator;
+    using GalaSoft.MvvmLight.Messaging;
+    using SkiRental.WPF.BL;
+    using SkiRental.WPF.UI;
 
     /// <summary>
     /// Interaction logic for App.xaml.
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// </summary>
+        public App()
+        {
+            ServiceLocator.SetLocatorProvider(() => MyIoC.Instance);
+
+            MyIoC.Instance.Register<IEditorService, EditorServiceViaWindow>();
+
+            MyIoC.Instance.Register<IMessenger>(() => Messenger.Default);
+
+            MyIoC.Instance.Register<ISkiEquipmentLogic, SkiEquipmentLogic>();
+        }
     }
 }
