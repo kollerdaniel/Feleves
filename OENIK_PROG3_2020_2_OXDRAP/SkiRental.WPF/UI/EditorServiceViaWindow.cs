709995e8be46873d4ad0c80c10918586a4e403f7
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2021-03-12 14:47:15 +0100

    Added UI layer in WPF

---
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/UI/EditorServiceViaWindow.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="EditorServiceViaWindow.cs" company="OXDRAP">
+// Copyright (c) OXDRAP. All rights reserved.
+// </copyright>
+
+namespace SkiRental.WPF.UI
+{
+    using SkiRental.WPF.BL;
+    using SkiRental.WPF.Data;
+
+    /// <summary>
+    /// This is the editor service via window class.
+    /// </summary>
+    public class EditorServiceViaWindow : IEditorService
+    {
+        /// <inheritdoc/>
+        public bool EditSkiEquipment(SkiEquipment s)
+        {
+            EditorWindow win = new EditorWindow(s);
+            return win.ShowDialog() ?? false;
+        }
+    }
+}
---
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/UI/ManufacturerToBrushConverter.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="ManufacturerToBrushConverter.cs" company="OXDRAP">
+// Copyright (c) OXDRAP. All rights reserved.
+// </copyright>
+
+namespace SkiRental.WPF.UI
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Media;
+    using SkiRental.WPF.Data;
+
+    /// <summary>
+    /// It is a manufacturer converter class.
+    /// </summary>
+    public class ManufacturerToBrushConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ManufacturerType pos = (ManufacturerType)value;
+            switch (pos)
+            {
+                default:
+                case ManufacturerType.Head: return Brushes.LightGreen;
+                case ManufacturerType.Atomic: return Brushes.Salmon;
+                case ManufacturerType.Rossignol: return Brushes.LightGray;
+            }
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
