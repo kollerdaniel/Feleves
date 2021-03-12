ecb2c5fbd026c7cbd5be129bd766abd0e08e4973
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2021-03-12 10:46:54 +0100

    Added an interface for the logic

---
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/BL/ISkiEquipmentLogic.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="ISkiEquipmentLogic.cs" company="OXDRAP">
+// Copyright (c) OXDRAP. All rights reserved.
+// </copyright>
+
+namespace SkiRental.WPF.BL
+{
+    using System.Collections.Generic;
+    using SkiRental.WPF.Data;
+
+    /// <summary>
+    /// It is an interface of ski equipment logic.
+    /// </summary>
+    public interface ISkiEquipmentLogic
+    {
+        /// <summary>
+        /// This method create an object.
+        /// </summary>
+        /// <param name="list">A list of ski equipments.</param>
+        void AddSkiEquipment(IList<SkiEquipment> list);
+
+        /// <summary>
+        /// This method modifis an object.
+        /// </summary>
+        /// <param name="skiEquipmentModify">A ski equipment object.</param>
+        void ModSkiEquipment(SkiEquipment skiEquipmentModify);
+
+        /// <summary>
+        /// This method deletes an object.
+        /// </summary>
+        /// <param name="list">A list oof ski equipments.</param>
+        /// <param name="skiEquipment">A ski equipment object.</param>
+        void DeleteSkiEquipment(IList<SkiEquipment> list, SkiEquipment skiEquipment);
+
+        /// <summary>
+        /// This is the get all method.
+        /// </summary>
+        /// <returns>It returns a list of ski equipments.</returns>
+        IList<SkiEquipment> GetAllSkiEquipment();
+    }
+}
