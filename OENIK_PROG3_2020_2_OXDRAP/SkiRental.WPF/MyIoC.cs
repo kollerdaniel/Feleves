5e4d8746f1f1113bea899a7993fd22f3dba8066f
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2021-03-12 17:43:44 +0100

    Added MyIoC

---
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/MyIoC.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="MyIoC.cs" company="OXDRAP">
+// Copyright (c) OXDRAP. All rights reserved.
+// </copyright>
+
+namespace SkiRental.WPF
+{
+    using CommonServiceLocator;
+    using GalaSoft.MvvmLight.Ioc;
+
+    /// <summary>
+    /// My inversion of control.
+    /// </summary>
+    public class MyIoC : SimpleIoc, IServiceLocator
+    {
+        /// <summary>
+        /// Gets the instance of my ioc.
+        /// </summary>
+        public static MyIoC Instance { get; private set; } = new MyIoC();
+    }
+}
