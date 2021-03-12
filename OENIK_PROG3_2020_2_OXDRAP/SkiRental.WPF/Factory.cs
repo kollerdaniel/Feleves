9288f434c1ad228be5bff6508dcb01e9e0f57a75
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2021-03-12 20:52:43 +0100

    Added comments

--- OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/Factory.cs
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/Factory.cs
@@ -6,6 +6,9 @@ namespace SkiRental.WPF
 {
     using SkiRental.Data;
 
+    /// <summary>
+    /// This is the factory class.
+    /// </summary>
     public class Factory
     {
         private SkiRentalContext ctx;
@@ -18,6 +21,9 @@ namespace SkiRental.WPF
             this.Ctx = new SkiRentalContext();
         }
 
+        /// <summary>
+        /// Gets or sets the datbase.
+        /// </summary>
         public SkiRentalContext Ctx { get => this.ctx; set => this.ctx = value; }
     }
 }
