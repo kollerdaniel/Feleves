5bf7d0c0fcc5cea9b1b9c11b4bac800cf716cb62
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2021-03-12 14:46:32 +0100

    Added enums to data layer

---
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/Data/DifficultyType.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="DifficultyType.cs" company="OXDRAP">
+// Copyright (c) OXDRAP. All rights reserved.
+// </copyright>
+
+namespace SkiRental.WPF.Data
+{
+    /// <summary>
+    /// Enum for difficulties.
+    /// </summary>
+    public enum DifficultyType
+    {
+        /// <summary>
+        /// Beginner difficulty.
+        /// </summary>
+        Beginner,
+
+        /// <summary>
+        /// Advanced difficulty.
+        /// </summary>
+        Advanced,
+
+        /// <summary>
+        /// Pro difficulty.
+        /// </summary>
+        Pro,
+    }
+}
---
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/Data/ManufacturerType.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ManufacturerType.cs" company="OXDRAP">
+// Copyright (c) OXDRAP. All rights reserved.
+// </copyright>
+
+namespace SkiRental.WPF.Data
+{
+    /// <summary>
+    /// Enum for manufacturers.
+    /// </summary>
+    public enum ManufacturerType
+    {
+        /// <summary>
+        /// Head manufacturer.
+        /// </summary>
+        Head,
+
+        /// <summary>
+        /// Atomic manufacturer.
+        /// </summary>
+        Atomic,
+
+        /// <summary>
+        /// Rossignol manufacturer.
+        /// </summary>
+        Rossignol,
+    }
+}
