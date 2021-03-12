ca0b8f3da2e0d3f9c77cfedcfec9626498c807ac
OXDRAP <koller.daniel@stud.uni-obuda.hu>
2021-03-12 10:48:04 +0100

    Added IEditorService interface

---
+++ OENIK_PROG3_2020_2_OXDRAP/SkiRental.WPF/BL/IEditorService.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IEditorService.cs" company="OXDRAP">
+// Copyright (c) OXDRAP. All rights reserved.
+// </copyright>
+
+namespace SkiRental.WPF.BL
+{
+    using SkiRental.WPF.Data;
+
+    /// <summary>
+    /// This is an important interface for business logic.
+    /// </summary>
+    public interface IEditorService
+    {
+        /// <summary>
+        /// Whit this, I can edit a ski equipment.
+        /// </summary>
+        /// <param name="s">A ski equipment object.</param>
+        /// <returns>Okay or cancel.</returns>
+        bool EditSkiEquipment(SkiEquipment s);
+    }
+}
